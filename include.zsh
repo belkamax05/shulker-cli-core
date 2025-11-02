dfb5b8b94a9763f76bb1ddf29725666cb4ffc714
Maksym Bondarchuk <belkamax05@gmail.com>
2025-11-02 05:33:03 +0000

    sync

--- include.zsh
+++ include.zsh
@@ -3,3 +3,4 @@ __CURRENT_DIR=$(realpath $(dirname $__CURRENT_FILE))
 
 SHULKER_CLI_CORE_DIR=$__CURRENT_DIR
 source "$SHULKER_CLI_CORE_DIR/functions/autoload-dir"
+autoload-dir "$SHULKER_CLI_CORE_DIR/functions"
