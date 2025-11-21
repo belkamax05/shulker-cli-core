078c54eccbfee6f24868ad5c7b9a2192948df520
Maksym Bondarchuk <belkamax05@gmail.com>
2025-11-21 03:05:43 +0000

    latest

--- include.zshenv.zsh
+++ include.zshenv.zsh
@@ -4,3 +4,5 @@ __CURRENT_DIR=$(realpath $(dirname $__CURRENT_FILE))
 SHULKER_CLI_CORE_DIR=$__CURRENT_DIR
 source "$SHULKER_CLI_CORE_DIR/functions/autoload-dir"
 autoload-dir "$SHULKER_CLI_CORE_DIR/functions"
+
+SHU_START_TIME=$(get-time-ms)
